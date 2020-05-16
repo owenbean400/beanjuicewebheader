c67b33bdc6c9840913814f4698efe03247fe4067
Owen Bean <63262699+owenbean400@users.noreply.github.com>
2020-05-16 13:40:43 -0400

    Add files via upload

--- beancart.js
+++ beancart.js
@@ -1,116 +1,116 @@
-var demonQty = sessionStorage.getItem("demon").match(/\d+$/g)[0];
-var sunshineQty = sessionStorage.getItem("sunshine").match(/\d+$/g)[0];
-var icyQty = sessionStorage.getItem("icy").match(/\d+$/g)[0];
-var demonName = sessionStorage.getItem("demon").match(/\D+/g)[0];
-var sunshineName = sessionStorage.getItem("sunshine").match(/\D+/g)[0];
-var icyName = sessionStorage.getItem("icy").match(/\D+/g)[0];
-var qty = sessionStorage.getItem("qty");
-var beanjuice = " BeanJuice"
-var amount = 69.99
 
-console.log(demonQty);
-console.log(sunshineQty);
-console.log(icyQty);
-console.log(demonName);
-console.log(sunshineName);
-console.log(icyName);
-console.log(qty);
+//variables
+    var demonQty = sessionStorage.getItem("demon").match(/\d+$/g)[0];
+    var sunshineQty = sessionStorage.getItem("sunshine").match(/\d+$/g)[0];
+    var icyQty = sessionStorage.getItem("icy").match(/\d+$/g)[0];
+    var demonName = sessionStorage.getItem("demon").match(/\D+/g)[0];
+    var sunshineName = sessionStorage.getItem("sunshine").match(/\D+/g)[0];
+    var icyName = sessionStorage.getItem("icy").match(/\D+/g)[0];
+    var qty = sessionStorage.getItem("qty");
+    var beanjuice = " BeanJuice"
+    var priceOne = 69.99
+    var priceDeal = 420.69
+    var dealAmountPerQuanity = 10
 
-$(function(){
-    createTable();
-    deleteTableHead();
-
-    $(".delete").click(function(){
-        if($(this).hasClass( "demon" )){
-            qty -= demonQty
-            demonQty -= demonQty
-            sessionStorage.setItem("qty", qty);
-            sessionStorage.setItem("demon","demon0");
-        }
-        else if($(this).hasClass( "sunshine" )){
-            qty -= sunshineQty
-            sunshineQty -= sunshineQty
-            sessionStorage.setItem("qty", qty);
-            sessionStorage.setItem("sunshine","sunshine0");
-        }
-        else if($(this).hasClass( "icy" )){
-            qty -= icyQty
-            icyQty -= icyQty
-            sessionStorage.setItem("qty", qty);
-            sessionStorage.setItem("icy","icy0");
-        }
-        $(this).parent().remove();
-        $("[id='quantity']").text(qty);
-        $("#price").text("$" + (amountIs(icyQty) + amountIs(sunshineQty) + amountIs(demonQty)));
-        cartAmount();
+//HTML loaded function
+    $(function(){
+        createTable();
         deleteTableHead();
 
+//Hit delete button
+        $(".delete").click(function(){
+            if($(this).hasClass( "demon" )){
+                qty -= demonQty
+                demonQty -= demonQty
+                sessionStorage.setItem("qty", qty);
+                sessionStorage.setItem("demon","demon0");
+            }
+            else if($(this).hasClass( "sunshine" )){
+                qty -= sunshineQty
+                sunshineQty -= sunshineQty
+                sessionStorage.setItem("qty", qty);
+                sessionStorage.setItem("sunshine","sunshine0");
+            }
+            else if($(this).hasClass( "icy" )){
+                qty -= icyQty
+                icyQty -= icyQty
+                sessionStorage.setItem("qty", qty);
+                sessionStorage.setItem("icy","icy0");
+            }
+            $(this).parent().remove();
+            $("[id='quantity']").text(qty);
+            $("#price").text("$" + (amountIs(icyQty) + amountIs(sunshineQty) + amountIs(demonQty)));
+            cartAmount();
+            deleteTableHead();
 
 
+
+        });
     });
-});
 
+//create table through jQuery
+    function createTable(){
+        qty = sessionStorage.getItem("qty");
+        amountIs();
+        if(demonQty > 0){
+            $("table").append("<tr> </tr>");
+            $("tr:last").append('<td><img src="photo/redcanweb.png"> </td>');
+            $("tr:last").append("<td>" + demonQty + "</td>");
+            $("tr:last").append("<td>" + demonName.charAt(0).toUpperCase() + demonName.slice(1) + beanjuice + "</td>");
+            $("tr:last").append("<td>" + "$" + (amountIs(demonQty)) + "</td>");
+            $("tr:last").append("<td> Delete </td>");
+            $("td:last").addClass("delete demon ");
+        }
+        if(sunshineQty > 0){
+            $("table").append("<tr> </tr>");
+            $("tr:last").append('<td><img src="photo/yellowcanweb.png"> </td>');
+            $("tr:last").append("<td>" + sunshineQty + "</td>");
+            $("tr:last").append("<td>" + sunshineName.charAt(0).toUpperCase() + sunshineName.slice(1) + beanjuice + "</td>");
+            $("tr:last").append("<td>" + "$" + (amountIs(sunshineQty)) + "</td>");
+            $("tr:last").append("<td> Delete </td>");
+            $("td:last").addClass("delete sunshine ");
+        }
+        if(icyQty > 0){
+            $("table").append("<tr> </tr>");
+            $("tr:last").append('<td><img src="photo/bluecanweb.png"> </td>');
+            $("tr:last").append("<td>" + icyQty + "</td>");
+            $("tr:last").append("<td>" + icyName.charAt(0).toUpperCase() + icyName.slice(1) + beanjuice + "</td>");
+            $("tr:last").append("<td>" + "$" + (amountIs(icyQty)) + "</td>");
+            $("tr:last").append("<td> Delete </td>");
+            $("td:last").addClass("delete icy ");
+        }
+        if(qty > 0){
+            $("table").append("<tr> </tr>");
+            $("tr:last").append("<td> Total </td>");
+            $("tr:last").append("<td>" + qty + "</td>");
+            $('td:last').attr('id', 'quantity');
+            $("tr:last").append("<td> </td>");
+            $("tr:last").append("<td>" + "$" + ( amountIs(icyQty) + amountIs(sunshineQty) + amountIs(demonQty) ).toFixed(2) + "</td>");
+            $('td:last').attr('id', 'price');
+        }
+        $("table").find("img").addClass("tableimg");
+    }
 
-function createTable(){
-    qty = sessionStorage.getItem("qty");
-    amountIs();
-    if(demonQty > 0){
-        $("table").append("<tr> </tr>");
-        $("tr:last").append('<td><img src="photo/redcanweb.png"> </td>');
-        $("tr:last").append("<td>" + demonQty + "</td>");
-        $("tr:last").append("<td>" + demonName.charAt(0).toUpperCase() + demonName.slice(1) + beanjuice + "</td>");
-        $("tr:last").append("<td>" + "$" + (amountIs(demonQty)) + "</td>");
-        $("tr:last").append("<td> Delete </td>");
-        $("td:last").addClass("delete demon ");
+//If no table, deletes headers
+    function deleteTableHead(){
+        if(qty <= 0){
+            $("tr:first").remove();
+            $("tr:last").remove();
+        }
     }
-    if(sunshineQty > 0){
-        $("table").append("<tr> </tr>");
-        $("tr:last").append('<td><img src="photo/yellowcanweb.png"> </td>');
-        $("tr:last").append("<td>" + sunshineQty + "</td>");
-        $("tr:last").append("<td>" + sunshineName.charAt(0).toUpperCase() + sunshineName.slice(1) + beanjuice + "</td>");
-        $("tr:last").append("<td>" + "$" + (amountIs(sunshineQty)) + "</td>");
-        $("tr:last").append("<td> Delete </td>");
-        $("td:last").addClass("delete sunshine ");
-    }
-    if(icyQty > 0){
-        $("table").append("<tr> </tr>");
-        $("tr:last").append('<td><img src="photo/bluecanweb.png"> </td>');
-        $("tr:last").append("<td>" + icyQty + "</td>");
-        $("tr:last").append("<td>" + icyName.charAt(0).toUpperCase() + icyName.slice(1) + beanjuice + "</td>");
-        $("tr:last").append("<td>" + "$" + (amountIs(icyQty)) + "</td>");
-        $("tr:last").append("<td> Delete </td>");
-        $("td:last").addClass("delete icy ");
-    }
-    if(qty > 0){
-        $("table").append("<tr> </tr>");
-        $("tr:last").append("<td> Total </td>");
-        $("tr:last").append("<td>" + qty + "</td>");
-        $('td:last').attr('id', 'quantity');
-        $("tr:last").append("<td> </td>");
-        $("tr:last").append("<td>" + "$" + ( amountIs(icyQty) + amountIs(sunshineQty) + amountIs(demonQty) ).toFixed(2) + "</td>");
-        $('td:last').attr('id', 'price');
-    }
-    $("table").find("img").addClass("tableimg");
-}
 
-function deleteTableHead(){
-    if(qty <= 0){
-        $("tr:first").remove();
-        $("tr:last").remove();
+//Price based on 
+    function amountIs(num){
+        if(num == 1){
+            return priceOne;
+        }
+        else if(num == dealAmountPerQuanity){
+            return priceDeal;
+        }
+        else if(num == 0){
+            return 0;
+        }
+        else{
+            return 5402.45
+        }
     }
-}
-
-function amountIs(num){
-    if(num == 1){
-        return 69.69;
-    }
-    else if(num == 10){
-        return 420.69;
-    }
-    else if(num == 0){
-        return 0;
-    }
-    else{
-        return 5402.45
-    }
-}
--- beanmaster.js
+++ beanmaster.js
@@ -1,35 +1,5 @@
-
-    var price = 69.69
-    var priceDeal = 420.69
-//set value for images    
-    var image = {
-        red: {
-            one: "photo/product/red1.jpg",
-            two: "photo/product/red2.jpg",
-            three: "photo/product/red3.jpg",
-            four: "photo/product/red4.jpg",
-            five: "photo/product/red5.jpg"
-        },
-        yellow: {
-            one: "photo/product/yellow1.jpg",
-            two: "photo/product/yellow2.jpg",
-            three: "photo/product/yellow3.jpg",
-            four: "photo/product/yellow4.jpg",
-            five: "photo/product/yellow5.jpg"
-        },
-        blue: {
-            one: "photo/product/blue1.jpg",
-            two: "photo/product/blue2.jpg",
-            three: "photo/product/blue3.jpg",
-            four: "photo/product/blue4.jpg",
-            five: "photo/product/blue5.jpg"
-        }
-    }
 //load window    
     function loading() {
-
-//variables
-        var showing = true;
 
 //function execute when page loads
         qtyCartNum();
@@ -43,22 +13,22 @@
                 
             });
 
-}
-
-
-function qtyCartNum(){
-    if(!window.sessionStorage.getItem('qty')){
-        window.sessionStorage.setItem('qty', 0);
-        window.sessionStorage.setItem('demon', "demon0");
-        window.sessionStorage.setItem('sunshine', "sunshine0");
-        window.sessionStorage.setItem('icy', "sunshine0");
     }
-    else{
-        window.sessionStorage.setItem('qty', window.sessionStorage.getItem('qty'));
+
+//If items not set, then set the items
+    function qtyCartNum(){
+        if(!window.sessionStorage.getItem('qty')){
+            window.sessionStorage.setItem('qty', 0);
+            window.sessionStorage.setItem('demon', "demon0");
+            window.sessionStorage.setItem('sunshine', "sunshine0");
+            window.sessionStorage.setItem('icy', "sunshine0");
+        }
+        else{
+            window.sessionStorage.setItem('qty', window.sessionStorage.getItem('qty'));
+        }
     }
-}
 
-
-function cartAmount(){
-    $("#qty").text(window.sessionStorage.getItem('qty'));
-}
+//Set cart number display to quantity
+    function cartAmount(){
+        $("#qty").text(window.sessionStorage.getItem('qty'));
+    }
